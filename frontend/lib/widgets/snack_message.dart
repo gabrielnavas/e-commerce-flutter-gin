98230904053312708c9bf281eefbf03f2f77cd3c
navas <gabrielnavas@email.com>
2024-04-10 21:02:01 -0300

    create snack message

---
+++ frontend/lib/widgets/snack_message.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SnackMessage {
+  static void show(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          message,
+          style: const TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
