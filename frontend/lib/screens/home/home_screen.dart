142c957e2ed5992685587fe0d5b859206101daf0
navas <gabrielnavas@email.com>
2024-04-11 22:32:12 -0300

    fashion banner

--- frontend/lib/screens/home/home_screen.dart
+++ frontend/lib/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:frontend/providers/auth_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -7,19 +8,59 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<AuthProvider>(context);
+    final width = MediaQuery.of(context).size.width;
+
     return Scaffold(
-        // appBar: AppBar(
-        //   actions: [
-        //     IconButton(
-        //       onPressed: () => provider.logoff(),
-        //       icon: const Icon(Icons.logout),
-        //     )
-        //   ],
-        //   title: const Text('title'),
-        // ),
-        body: Column(
-      children: [Image.asset('assets/images/home_fashion_sale.png')],
-    ));
+      body: Column(
+        children: [
+          _fashionBanner(
+            width,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Stack _fashionBanner(double width) {
+    return Stack(
+      children: [
+        SizedBox(
+          width: double.infinity,
+          child: Image.asset(
+            'assets/images/home_fashion_sale.png',
+            fit: BoxFit.cover,
+          ),
+        ),
+        Positioned(
+          left: 15,
+          bottom: 30,
+          child: SizedBox(
+            width: width * 0.50,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Fashion Sale',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: width * 0.10,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: width * 0.15),
+                  ),
+                  child: const Text(
+                    'Check',
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
