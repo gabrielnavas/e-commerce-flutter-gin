8e75b175f66d7adfb6472153496f9010ff2c656d
navas <gabrielnavas@email.com>
2024-04-15 21:12:56 -0300

    add color test

--- backend/internal/core/product/models/color.go
+++ backend/internal/core/product/models/color.go
@@ -1,10 +1,32 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"time"
+)
+
+var (
+	ErrNameColorIsEmpty              = errors.New("name is empty")
+	ErrNameColorMustHaveOneCharacter = errors.New("name must have at least one character")
+	ErrNameColorNameIsLong           = errors.New("name is too long")
+)
 
 type Color struct {
 	ID        string
 	Name      string
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+func (c *Color) Validate() error {
+	if len(c.Name) == 0 {
+		return ErrNameColorIsEmpty
+	}
+	if len(c.Name) < 2 {
+		return ErrNameColorMustHaveOneCharacter
+	}
+	if len(c.Name) > 50 {
+		return ErrNameColorNameIsLong
+	}
+	return nil
 }
---
+++ backend/internal/core/product/models/color_test.go
@@ -0,0 +1,48 @@
+package models_test
+
+import (
+	"ecommerce/internal/core/product/models"
+	"testing"
+	"time"
+)
+
+func TestColor(t *testing.T) {
+	testCases := colorUseCaseTests()
+
+	for _, testCase := range testCases {
+		received := testCase.data.Validate()
+
+		if received != testCase.expected {
+			t.Errorf("received %v expected %v", received, testCase.expected)
+		}
+	}
+}
+
+func colorUseCaseTests() []UseCaseTest[models.Color] {
+	return []UseCaseTest[models.Color]{
+		{
+			data: models.Color{
+				ID:        "1",
+				Name:      "",
+				CreatedAt: time.Now(),
+			},
+			expected: models.ErrNameColorIsEmpty,
+		},
+		{
+			data: models.Color{
+				ID:        "2",
+				Name:      "1",
+				CreatedAt: time.Now(),
+			},
+			expected: models.ErrNameColorMustHaveOneCharacter,
+		},
+		{
+			data: models.Color{
+				ID:        "3",
+				Name:      anyLongString(51),
+				CreatedAt: time.Now(),
+			},
+			expected: models.ErrNameColorNameIsLong,
+		},
+	}
+}
